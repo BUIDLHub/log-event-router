abf437ef6922ab0fbe1d60ca1b09d2573b87504b
mdcoon <mdcoon1@yahoo.com>
2019-05-04 06:04:43 -0400

    Cleaned up code a bit

--- packages/demo/src/Redux/events/operations.js
+++ packages/demo/src/Redux/events/operations.js
@@ -2,56 +2,64 @@ import {Creators} from './actions';
 import _ from 'lodash';
 
 const addType = (byType,event) => {
-
   let a = byType[event.event] || [];
   a.push(event);
   byType[event.event] = a;
 }
 
+const storeEvents = async (events, storage) => {
+  let byDB = {};
+  for(let i=0;i<events.length;++i) {
+    let evt = events[i];
+    let items = byDB[evt.event] || [];
+    items.push({
+      key: evt.id,
+      value: evt
+    });
+    byDB[evt.event] = items;
+  }
+  let dbs = _.keys(byDB);
+  for(let i=0;i<dbs.length;++i) {
+    let db = dbs[i];
+    let items = byDB[db];
+    await storage.storeBulk({
+      database: db,
+      items
+    });
+  }
+}
+
 const init = () => async (dispatch,getState) => {
   let stream = getState().stream.streams;
-  stream.use("breedWithAuto", async (ctx,next)=>{
-    console.log("Breeding txn", ctx.txn);
-    let events = _.values(ctx.txn.logEvents);
 
+  //runs on all txns
+  stream.use(async (ctx, next)=>{
     let state = getState();
     let byType = {
       ...state.events.byType
     };
-
+    let events = _.values(ctx.txn.logEvents);
     for(let i=0;i<events.length;++i) {
       let evt = events[i];
-
-      ctx.storage.store({
-        database: evt.event,
-        key: evt.id,
-        data: evt
-      });
-      addType(byType,evt)
+      addType(byType, evt);
     }
     dispatch(Creators.update(byType));
     next();
   });
 
-  stream.use("giveBirth", (ctx,next)=>{
+  //txns where contract's breedWithAuto fn was called
+  stream.use("breedWithAuto", async (ctx,next)=>{
+    console.log("Breeding txn", ctx.txn);
+    let events = _.values(ctx.txn.logEvents);
+    await storeEvents(events, ctx.storage);
+    next();
+  });
+
+  //txns where contract's giveBirth fn was called
+  stream.use("giveBirth", async (ctx,next)=>{
     console.log("Birth txn", ctx.txn);
     let events = _.values(ctx.txn.logEvents);
-
-    let state = getState();
-    let byType = {
-      ...state.events.byType
-    };
-    for(let i=0;i<events.length;++i) {
-      let evt = events[i];
-
-      ctx.storage.store({
-        database: evt.event,
-        key: evt.id,
-        data: evt
-      });
-      addType(byType,evt)
-    }
-    dispatch(Creators.update(byType));
+    await storeEvents(events, ctx.storage);
     next();
   });
 }
